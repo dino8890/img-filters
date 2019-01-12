6c587f596d5fba44ec9e9e7c4127f0cca7d1ed62
Dino <dino8890@gmail.com>
2019-01-12 16:15:31 +0100

    implemented GPU blur filter

--- filters/gpu/blur.cu
+++ filters/gpu/blur.cu
@@ -1,28 +1,20 @@
-__global__ void apply_filter(unsigned char *input_channel, unsigned char *output_channel, unsigned char *buffer,
-                             unsigned int width, unsigned int height,
-                             float *gaussian_kernel, float standard_deviation, unsigned int filter_width) {
-
-    unsigned int blockId = blockIdx.x + blockIdx.y * gridDim.x;
-    unsigned int threadId = blockId * (blockDim.x * blockDim.y) + (threadIdx.x + threadIdx.y * blockDim.x);
-    if(threadId < width * height) {
-        unsigned int row = threadIdx.y + blockIdx.y * gridDim.y;
-        unsigned int col = threadIdx.x + blockIdx.x * gridDim.x;
-
-        buffer[col + row * height] = input_channel[threadId];
-
-        __syncthreads();
-
+__global__ void apply_filter(const unsigned char *input_channel, unsigned char *output_channel,
+                             const unsigned int width, const unsigned int height,
+                             const float *gaussian_kernel, const unsigned int filter_width) {
+    const unsigned int row = threadIdx.y + blockIdx.y * blockDim.y;
+    const unsigned int col = threadIdx.x + blockIdx.x * blockDim.x;
+    if(row < height && col < width) {
+        const int filter_half = filter_width / 2;
         float blur = 0.0;
-        int filter_half = filter_width / 2;
         for(int i = -filter_half; i <= filter_half; i++) {
             for(int j = -filter_half; j <= filter_half; j++) {
-                unsigned int x = max(0, min(height - 1, col + j));
-                unsigned int y = max(0, min(width - 1, row + i));
+                const unsigned int y = max(0, min(height - 1, row + i));
+                const unsigned int x = max(0, min(width - 1, col + j));
 
-                blur += buffer[x + y * height] * gaussian_kernel[(j + filter_half) + (i + filter_half) * filter_width];
+                const float w = gaussian_kernel[(j + filter_half) + (i + filter_half) * filter_width];
+                blur += w * input_channel[x + y * width];
             }
         }
-
-        output_channel[threadId] = static_cast<unsigned char>(blur);
+        output_channel[col + row * width] = static_cast<unsigned char>(blur);
     }
 }
--- filters/gpu/blur.py
+++ filters/gpu/blur.py
@@ -9,18 +9,6 @@ DIM_BLOCK = 32
 CUDA_KERNEL_CODE_PATH = 'filters/gpu/blur.cu'
 
 
-def _nearest_multiple(a, b):
-    return int(math.ceil(a / b) * b)
-
-
-def _largest_factors(x):
-    i = x - 1
-    while x % i != 0:
-        i -= 1
-
-    return int(i), int(x / i)
-
-
 def _create_gaussian_kernel(filter_width, standard_deviation):
     matrix = np.empty((filter_width, filter_width), np.float32)
     filter_half = filter_width // 2
@@ -42,9 +30,8 @@ def apply(source_array, standard_deviation, filter_width):
 
     height, width = source_array.shape[:2]
 
-    num_blocks = (_nearest_multiple(width, DIM_BLOCK)
-                  * _nearest_multiple(height, DIM_BLOCK)
-                  / DIM_BLOCK**2)
+    dim_grid_x = math.ceil(width / DIM_BLOCK)
+    dim_grid_y = math.ceil(height / DIM_BLOCK)
 
     max_num_blocks = (
             pycuda.autoinit.device.get_attribute(
@@ -55,12 +42,11 @@ def apply(source_array, standard_deviation, filter_width):
             )
     )
 
-    if num_blocks > max_num_blocks:
+    if (dim_grid_x * dim_grid_y) > max_num_blocks:
         raise ValueError(
             'image dimensions too great, maximum block number exceeded'
         )
 
-    dim_grid_x, dim_grid_y = _largest_factors(num_blocks)
     gaussian_kernel = _create_gaussian_kernel(filter_width, standard_deviation)
 
     mod = compiler.SourceModule(open(CUDA_KERNEL_CODE_PATH).read())
@@ -70,12 +56,10 @@ def apply(source_array, standard_deviation, filter_width):
         apply_filter(
             driver.In(channel),
             driver.Out(channel),
-            driver.In(np.full_like(channel, 255)),
-            np.uint32(width),
-            np.uint32(height),
+            np.int32(width),
+            np.int32(height),
             driver.In(gaussian_kernel),
-            np.float32(standard_deviation),
-            np.uint32(filter_width),
+            np.int32(filter_width),
             block=(DIM_BLOCK, DIM_BLOCK, 1),
             grid=(dim_grid_x, dim_grid_y)
         )
